13fbbf596f2d90c8dfbe260cd1f2911dcb2d1b00
Ralepel <45490824+Ralepel@users.noreply.github.com>
2021-01-05 23:56:56 +0100

    MyAgent geprogrammeerd

--- hoofdopdracht bke machine learning.py
+++ hoofdopdracht bke machine learning.py
@@ -1,1 +1,11 @@
 from bke import MLAgent, is_winner, opponent, train, validate, RandomAgent, plot_validation
+
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
